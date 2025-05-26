23e385083e9d81c00fe3d843e49ca21de09b3cdf
AsmaAbid <asmahabid2006@gmail.com>
2025-05-26 15:19:51 +0500

    Add files via upload

---
+++ VehicleDetection.py
@@ -0,0 +1,24 @@
+import cv2
+from ultralytics import YOLO
+model = YOLO('yolov8n.pt') 
+video_path = 'Task7/videos/video2.mp4'  
+cap = cv2.VideoCapture(video_path)
+if not cap.isOpened():
+    print("Error opening video file")
+    exit()
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+out = cv2.VideoWriter('output_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    out.write(annotated_frame)
+    cv2.imshow('YOLOv8 Object Detection', annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
